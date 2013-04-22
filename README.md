8e3057dc795e4fa6d3c30860f1746d0f86d47202
jimmay5469 <jimmay5469@gmail.com>
2013-04-22 11:06:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Time Tracker
+============
+
+Personal time logger for daily activities.
+
+
+Setup
+-----
+1. Install Git, Node.js, NPM.
+2. Clone this repository: `git clone [url]`.
+3. Run `npm install` in the root.
+4. Start the project using `node server.js`.
---
+++ server.js
@@ -0,0 +1,13 @@
+var application_root = __dirname;
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.configure(function () {
+	app.use(express.static(path.join(application_root, 'public')));
+});
+
+app.listen(process.env.VMC_APP_PORT || 1337, null);
+
+console.log('Server running on http://localhost:1337/');
